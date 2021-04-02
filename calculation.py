e947980188647221b2750e07024dfdfea5e3bcf3
Naihrita Munshi <81513753+naihrita1987@users.noreply.github.com>
2021-04-02 12:45:18 +0200

    first commit

--- calculation.py
+++ calculation.py
@@ -1,4 +1,4 @@
-num1 = int(input("Enter 1st number:"))
+num1 = int(input("Enter 1st number:")) #first number
 num2 = int(input("Enter 2nd number:"))
 num3 = int(input("Enter 3rd number:"))
 num4 = int(input("Enter 4th number:"))
